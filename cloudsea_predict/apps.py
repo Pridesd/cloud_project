4545161bd4d4760374e1f727c9921eb573cd411b
Pridesd <whwotjr98@naver.com>
2023-06-14 13:25:26 +0900

    앱 추가

--- cloudsea_predict/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CloudseaPredictConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cloudsea_predict'
--- hiking_weather/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HikingWeatherConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hiking_weather'
